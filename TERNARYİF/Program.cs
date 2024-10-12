cc3a4e8a7005592b03b4d26178e97474ae9d87cb
USER <USER@LAPTOP-F5PIAQHS>
2024-10-12 18:59:04 +0300

    Add project files.

---
+++ TERNARYİF/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace TERNARYİF
+{
+    internal class Program
+    {          static void Main(string[] args)
+        {
+            /* Console.WriteLine("CİNSİYET GİRİNİZ: 1.ERKEK \n 2.KADIN");
+            var deger=Console.ReadLine();
+            var sonuc = deger =="1" ? "ERKEK" :"KADIN";
+            Console.WriteLine(sonuc);
+            */
+
+
+
+            //TERNARY İF : ÜÇLÜ EĞER 
+            //DEGER=KOŞUL?TRUE;FALSE 
+            //EĞER ATAMA YAPILACAKSA İF ELSE KULLANIRIZ.
+          Console.WriteLine("NOTUNUZU GİRİN :");
+            var not =Convert.ToInt32(Console.ReadLine());
+            
+            Console.WriteLine("NOTUNUZ {0}",not < 25 ? "FF-KALDI" : not >= 25 & not <= 50 ? "FD" : not >= 50 & not <= 60 ? "DD" : not >= 60 & not <= 70 ? "DC" : "AA-GEÇTİ"  );
+
+
+            // 
+
+            //1.DERLEME ZAMANI HATALAR:
+
+
+            
+        }
+    }
+}
